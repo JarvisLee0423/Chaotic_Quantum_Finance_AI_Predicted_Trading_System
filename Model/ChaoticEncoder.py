6a0db1d46e994d62fa39d11fc7e01ec84c778a2b
JarvisLee <609089938@qq.com>
2021-05-01 03:48:28 +0800

    Chaotic Encoder

---
+++ Model/ChaoticEncoder.py
@@ -0,0 +1,42 @@
+'''
+    Copyright:      JarvisLee
+    Date:           5/1/2021
+    File Name:      ChaoticEncoder.py
+    Description:    The Chaotic Bi-directional LSTM Unit based Encoder.
+'''
+
+# Import the necessary library.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ChaoticLSTM import ChaoticLSTM
+
+# Create the class for the Chaotic Encoder.
+class ChaoticEncoder(nn.Module):
+    # Create the constructor.
+    def __init__(self, inputSize, hiddenSize):
+        # Create the super constructor.
+        super(ChaoticEncoder, self).__init__()
+        # Create the Chaotic Encoder.
+        self.CLSTM = ChaoticLSTM(inputSize = inputSize, hiddenSize = hiddenSize)
+    
+    # Create the forward propagation.
+    def forward(self, x):
+        # Compute the Chaotic Long Short-Term Memory Unit.
+        output, hidden = self.CLSTM(x)
+        # Return the output and hidden.
+        return output, hidden
+
+# Test the Chaotic Encoder.
+if __name__ == "__main__":
+    # Create the Chaotic Encoder.
+    CEncoder = ChaoticEncoder(4, 10)
+    # Test the Chaotic Encoder.
+    x = torch.randn((32, 9, 4))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden[0].shape)
+    print(hidden[1].shape)
+    print(hidden[2].shape)
+    print(hidden[3].shape)
