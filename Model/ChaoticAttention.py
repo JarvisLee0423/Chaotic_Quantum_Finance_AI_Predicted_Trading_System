a26e14530f1aba70a4273f2cc21a44809642302e
JarvisLee <609089938@qq.com>
2021-05-01 06:12:24 +0800

    Chaotic Attention Mechanism

--- Model/ChaoticAttention.py
+++ Model/ChaoticAttention.py
@@ -10,7 +10,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from LeeOscillator import LeeOscillator
-from ChaoticEncoder import ChaoticEncoder
 
 # Create the class for the Lee-Oscillator based Attention Mechanism.
 class ChaoticAttention(nn.Module):
@@ -20,41 +19,33 @@ class ChaoticAttention(nn.Module):
             - hiddenSize (integer), The input and output size unit of the Chaotic Attention Mechanism.\n
     '''
     # Create the constructor.
-    def __init__(self, hiddenSize):
+    def __init__(self, inputSize, hiddenSize):
         # Create the super constructor.
         super(ChaoticAttention, self).__init__()
         # Create the linear layer.
-        self.fc = nn.Linear(6 * hiddenSize, 2 * hiddenSize)
+        self.fc = nn.Linear(inputSize, hiddenSize)
         # Create the Lee-Oscillator.
         self.Lee = LeeOscillator(compute = False)
     
     # Create the forward propagation.
     def forward(self, x, h, c, hinv, cinv):
-        # Set the list to store the alpha.
-        tempAlpha = []
         # Compute the alpha.
-        for t in range(x.shape[1]):
-            # Concatenate the x, hidden, cell, inverse hidden and inverse cell.
-            xcat = torch.cat([x[:, t, :].unsqueeze(1), h.unsqueeze(1), c.unsqueeze(1), hinv.unsqueeze(1), cinv.unsqueeze(1)], dim = 2)
-            # Store the temp alpha.
-            tempAlpha.append(xcat)
-        # Compute the alpha.
-        alpha = torch.cat(tempAlpha, dim = 1)
-        alpha = self.fc(alpha.reshape(-1, xcat.shape[2]))
+        alpha = torch.zeros((x.shape[0], x.shape[1], x.shape[2] + h.shape[1] + c.shape[1] + hinv.shape[1] + cinv.shape[1]))
+        alpha[:, :, :x.shape[2]] = x
+        alpha[:, :, x.shape[2]:] = torch.cat([h.unsqueeze(1), c.unsqueeze(1), hinv.unsqueeze(1), cinv.unsqueeze(1)], dim = 2)
+        alpha = self.fc(alpha.reshape(-1, alpha.shape[2]))
         alpha = self.Lee.Tanh(alpha).reshape(x.shape[0], x.shape[1], -1)
         # Compute the context.
         context = torch.sum(alpha * x, dim = 1)
         # Return the context.
         return context.unsqueeze(1)
 
-# Test the Chaotic Attention Mechanism.
+# Create the main function to test the Chaotic Attention Mechanism.
 if __name__ == "__main__":
     # Create the Chaotic Attention Mechanism.
-    CAttention = ChaoticAttention(hiddenSize = 10)
-    # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(inputSize = 4, hiddenSize = 10)
+    CAttention = ChaoticAttention(inputSize = 36, hiddenSize = 20)
     # Test the Chaotic Attention Mechanism.
-    x = torch.randn((32, 9, 4))
-    output, hidden = CEncoder(x)
-    context = CAttention(output, hidden[0], hidden[1], hidden[2], hidden[3])
+    x = torch.randn((32, 9, 20))
+    hidden = (torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)))
+    context = CAttention(x, hidden[0], hidden[1], hidden[2], hidden[3])
     print(context.shape)
