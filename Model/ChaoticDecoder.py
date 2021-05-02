04e719950c02f51f598a56950e35c4b4257649e3
JarvisLee <609089938@qq.com>
2021-05-02 15:24:34 +0800

    Chaotic Decoder

--- Model/ChaoticDecoder.py
+++ Model/ChaoticDecoder.py
@@ -19,10 +19,11 @@ class ChaoticDecoder(nn.Module):
         Params:\n
             - inputSize (integer), The input size of the Chaotic Decoder.\n
             - hiddenSize (integer), The output size of the Chaotic Decoder.\n
+            - outputSize (integer), The output size of the Chaotic Decoder.\n 
             - attention (ChaoticAttention), The Chaotic Attention.\n
     '''
     # Create the constructor.
-    def __init__(self, inputSize, hiddenSize):
+    def __init__(self, inputSize, hiddenSize, outputSize):
         # Create the super constructor.
         super(ChaoticDecoder, self).__init__()
         # Get the member variables.
@@ -32,6 +33,8 @@ class ChaoticDecoder(nn.Module):
         self.CAttention = ChaoticAttention(inputSize = inputSize + 4 * hiddenSize, hiddenSize = inputSize)
         # Create the Chaotic Decoder.
         self.CLSTM = ChaoticLSTM(inputSize = inputSize, hiddenSize = hiddenSize)
+        # Create the Fully Connected Layer.
+        self.fc = nn.Linear(2 * hiddenSize, outputSize)
     
     # Create the forward propagation.
     def forward(self, x):
@@ -47,14 +50,14 @@ class ChaoticDecoder(nn.Module):
             # Compute the lstm.
             output, (ht, ct, hinvt, cinvt) = self.CLSTM(context, (ht, ct, hinvt, cinvt))
         # Get the output.
-        output = output[:, :, :4] + output[:, :, 4:]
+        output = self.fc(output.squeeze())
         # Return the output.
-        return output.reshape(output.shape[0], 4, 1).squeeze()
+        return output
 
 # Create the main function to test the Chaotic Decoder.
 if __name__ == "__main__":
     # Create the Chaotic Decoder.
-    CDecoder = ChaoticDecoder(20, 4)
+    CDecoder = ChaoticDecoder(20, 20, 4)
     # Test the Chaotic Decoder.
     x = torch.randn((32, 9, 20))
     output = CDecoder(x)
