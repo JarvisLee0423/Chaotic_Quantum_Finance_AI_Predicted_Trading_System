62d88ce2b4332f8308278faf476d8b7d447ba07b
JarvisLee <609089938@qq.com>
2021-05-05 01:47:17 +0800

    Chaotic Bi-directional Long Short-Term Memory Unit

--- Model/ChaoticLSTM.py
+++ Model/ChaoticLSTM.py
@@ -19,14 +19,16 @@ class ChaoticLSTM(nn.Module):
         Params:\n
             - inputSize (integer), The input size of the Chaotic LSTM.\n
             - hiddenSize (integer), The output size of the Chaotic LSTM.\n
+            - chaotic (bool), The boolean to check whether use the Chaotic Mode.\n
     '''
     # Create the constructor.
-    def __init__(self, inputSize, hiddenSize):
+    def __init__(self, inputSize, hiddenSize, chaotic = True):
         # Create the super constructor.
         super(ChaoticLSTM, self).__init__()
         # Get the member variables.
         self.inputSize = inputSize
         self.hiddenSize = hiddenSize
+        self.chaotic = chaotic
         # Create the parameter of the input.
         self.Wi = nn.Parameter(torch.Tensor(inputSize, hiddenSize * 4))
         # Create the parameter of the hidden.
@@ -66,31 +68,58 @@ class ChaoticLSTM(nn.Module):
             # Compute the forward.
             gates = xt @ self.Wi + ht @ self.Wh + self.B
             # Get the value of each gate.
-            it, ft, gt, ot = (
-                self.Lee.Sigmoid(gates[:, :self.hiddenSize]).to(x.device),
-                self.Lee.Sigmoid(gates[:, self.hiddenSize:self.hiddenSize * 2]).to(x.device),
-                self.Lee.Tanh(gates[:, self.hiddenSize * 2:self.hiddenSize * 3]).to(x.device),
-                self.Lee.Sigmoid(gates[:, self.hiddenSize * 3:]).to(x.device)
-            )
-            # Compute the cell and hidden.
-            ct = ft * ct + it * gt
-            ht = ot * self.Lee.Tanh(ct).to(x.device)
-            # Store the forward value.
-            output[0].append(ht.unsqueeze(1))
-            # Compute the backward.
-            gatesInv = xinvt @ self.Wi + hinvt @ self.Wh + self.B
-            # Get the value of each inverse gate.
-            iinvt, finvt, ginvt, oinvt = (
-                self.Lee.Sigmoid(gatesInv[:, :self.hiddenSize]).to(x.device),
-                self.Lee.Sigmoid(gatesInv[:, self.hiddenSize:self.hiddenSize * 2]).to(x.device),
-                self.Lee.Tanh(gatesInv[:, self.hiddenSize * 2:self.hiddenSize * 3]).to(x.device),
-                self.Lee.Sigmoid(gatesInv[:, self.hiddenSize * 3:]).to(x.device)
-            )
-            # Compute the inverse cell and hidden.
-            cinvt = finvt * cinvt + iinvt * ginvt
-            hinvt = oinvt * self.Lee.Tanh(cinvt).to(x.device)
-            # Store the backward value.
-            output[1].append(hinvt.unsqueeze(1))
+            if self.chaotic == True:
+                it, ft, gt, ot = (
+                    self.Lee.Sigmoid(gates[:, :self.hiddenSize]).to(x.device),
+                    self.Lee.Sigmoid(gates[:, self.hiddenSize:self.hiddenSize * 2]).to(x.device),
+                    self.Lee.Tanh(gates[:, self.hiddenSize * 2:self.hiddenSize * 3]).to(x.device),
+                    self.Lee.Sigmoid(gates[:, self.hiddenSize * 3:]).to(x.device)
+                )
+                # Compute the cell and hidden.
+                ct = ft * ct + it * gt
+                ht = ot * self.Lee.Tanh(ct).to(x.device)
+                # Store the forward value.
+                output[0].append(ht.unsqueeze(1))
+                # Compute the backward.
+                gatesInv = xinvt @ self.Wi + hinvt @ self.Wh + self.B
+                # Get the value of each inverse gate.
+                iinvt, finvt, ginvt, oinvt = (
+                    self.Lee.Sigmoid(gatesInv[:, :self.hiddenSize]).to(x.device),
+                    self.Lee.Sigmoid(gatesInv[:, self.hiddenSize:self.hiddenSize * 2]).to(x.device),
+                    self.Lee.Tanh(gatesInv[:, self.hiddenSize * 2:self.hiddenSize * 3]).to(x.device),
+                    self.Lee.Sigmoid(gatesInv[:, self.hiddenSize * 3:]).to(x.device)
+                )
+                # Compute the inverse cell and hidden.
+                cinvt = finvt * cinvt + iinvt * ginvt
+                hinvt = oinvt * self.Lee.Tanh(cinvt).to(x.device)
+                # Store the backward value.
+                output[1].append(hinvt.unsqueeze(1))
+            else:
+                it, ft, gt, ot = (
+                    torch.sigmoid(gates[:, :self.hiddenSize]).to(x.device),
+                    torch.sigmoid(gates[:, self.hiddenSize:self.hiddenSize * 2]).to(x.device),
+                    torch.tanh(gates[:, self.hiddenSize * 2:self.hiddenSize * 3]).to(x.device),
+                    torch.sigmoid(gates[:, self.hiddenSize * 3:]).to(x.device)
+                )
+                # Compute the cell and hidden.
+                ct = ft * ct + it * gt
+                ht = ot * torch.tanh(ct).to(x.device)
+                # Store the forward value.
+                output[0].append(ht.unsqueeze(1))
+                # Compute the backward.
+                gatesInv = xinvt @ self.Wi + hinvt @ self.Wh + self.B
+                # Get the value of each inverse gate.
+                iinvt, finvt, ginvt, oinvt = (
+                    torch.sigmoid(gatesInv[:, :self.hiddenSize]).to(x.device),
+                    torch.sigmoid(gatesInv[:, self.hiddenSize:self.hiddenSize * 2]).to(x.device),
+                    torch.tanh(gatesInv[:, self.hiddenSize * 2:self.hiddenSize * 3]).to(x.device),
+                    torch.sigmoid(gatesInv[:, self.hiddenSize * 3:]).to(x.device)
+                )
+                # Compute the inverse cell and hidden.
+                cinvt = finvt * cinvt + iinvt * ginvt
+                hinvt = oinvt * torch.tanh(cinvt).to(x.device)
+                # Store the backward value.
+                output[1].append(hinvt.unsqueeze(1))
         # Concatenate the output, hidden and cell.
         output[0] = torch.cat(output[0], dim = 1)
         output[1] = torch.cat(output[1], dim = 1)
@@ -102,6 +131,18 @@ class ChaoticLSTM(nn.Module):
 if __name__ == "__main__":
     # Create the Chaotic LSTM unit.
     CLSTM = ChaoticLSTM(inputSize = 4, hiddenSize = 10)
+    # Test the Chaotic LSTM.
+    x = torch.randn((32, 9, 4))
+    print(x.shape)
+    output, (h, c, hinv, cinv) = CLSTM(x)
+    print(output.shape)
+    print(h.shape)
+    print(c.shape)
+    print(hinv.shape)
+    print(cinv.shape)
+
+    # Create the Chaotic LSTM unit.
+    CLSTM = ChaoticLSTM(inputSize = 4, hiddenSize = 10, chaotic = False)
     # Test the Chaotic LSTM.
     x = torch.randn((32, 9, 4))
     print(x.shape)
