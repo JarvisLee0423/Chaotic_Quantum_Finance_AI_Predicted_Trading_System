e205845f58a4cc070e750bce35e6ac9831905341
JarvisLee <609089938@qq.com>
2021-05-07 02:24:39 +0800

    Chaotic Decoder

--- Model/ChaoticDecoder.py
+++ Model/ChaoticDecoder.py
@@ -9,8 +9,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from ChaoticAttention import ChaoticAttention
-from ChaoticLSTM import ChaoticLSTM
+from Model.ChaoticAttention import ChaoticAttention
+from Model.ChaoticLSTM import ChaoticLSTM
+from Model.LeeOscillator import LeeOscillator
 
 # Create the class for the Chaotic Decoder.
 class ChaoticDecoder(nn.Module):
@@ -20,20 +21,21 @@ class ChaoticDecoder(nn.Module):
             - inputSize (integer), The input size of the Chaotic Decoder.\n
             - hiddenSize (integer), The output size of the Chaotic Decoder.\n
             - outputSize (integer), The output size of the Chaotic Decoder.\n 
+            - Lee (LeeOscillator), The Lee-Oscillator.\n
             - attention (ChaoticAttention), The Chaotic Attention.\n
             - chaotic (bool), The boolean to check whether use the Chaotic Mode.\n
     '''
     # Create the constructor.
-    def __init__(self, inputSize, hiddenSize, outputSize, chaotic = True):
+    def __init__(self, inputSize, hiddenSize, outputSize, Lee, chaotic = True):
         # Create the super constructor.
         super(ChaoticDecoder, self).__init__()
         # Get the member variables.
         self.inputSize = inputSize
         self.hiddenSize = hiddenSize
         # Create the Chaotic attention.
-        self.CAttention = ChaoticAttention(inputSize = inputSize + 4 * hiddenSize, hiddenSize = inputSize, chaotic = chaotic)
+        self.CAttention = ChaoticAttention(inputSize = inputSize + 4 * hiddenSize, hiddenSize = inputSize, Lee = Lee, chaotic = chaotic)
         # Create the Chaotic Decoder.
-        self.CLSTM = ChaoticLSTM(inputSize = inputSize, hiddenSize = hiddenSize, chaotic = chaotic)
+        self.CLSTM = ChaoticLSTM(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic)
         # Create the Fully Connected Layer.
         self.fc = nn.Linear(2 * hiddenSize, outputSize)
     
@@ -57,15 +59,17 @@ class ChaoticDecoder(nn.Module):
 
 # Create the main function to test the Chaotic Decoder.
 if __name__ == "__main__":
+    # Get the Lee-Oscillator.
+    Lee = LeeOscillator()
     # Create the Chaotic Decoder.
-    CDecoder = ChaoticDecoder(20, 20, 4)
+    CDecoder = ChaoticDecoder(20, 20, 4, Lee = Lee)
     # Test the Chaotic Decoder.
     x = torch.randn((32, 9, 20))
     output = CDecoder(x)
     print(output.shape)
 
     # Create the Chaotic Decoder.
-    CDecoder = ChaoticDecoder(20, 20, 4, chaotic = False)
+    CDecoder = ChaoticDecoder(20, 20, 4, Lee = Lee, chaotic = False)
     # Test the Chaotic Decoder.
     x = torch.randn((32, 9, 20))
     output = CDecoder(x)
