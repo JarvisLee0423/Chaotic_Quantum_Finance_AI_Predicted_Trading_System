b7fd6f631aa2cab626cca412e18bf58e1b1caa99
JarvisLee <609089938@qq.com>
2021-06-04 09:53:32 +0800

    Chaotic Attention Mechanism

--- Model/ChaoticAttention.py
+++ Model/ChaoticAttention.py
@@ -8,7 +8,6 @@
 # Import the necessary library.
 import torch
 import torch.nn as nn
-from Model.LeeOscillator import LeeOscillator
 
 # Create the class for the Lee-Oscillator based Attention Mechanism.
 class ChaoticAttention(nn.Module):
@@ -17,19 +16,17 @@ class ChaoticAttention(nn.Module):
         Params:\n
             - inputSize (integer), The input size of the Chaotic Attention Mechanism.\n
             - hiddenSize (integer), The output size of the Chaotic Attention Mechanism.\n
-            - Lee (LeeOscillator), The Lee-Oscillator.\n
-            - chaotic (bool), The boolean to check whether use the Chaotic Mode.\n
     '''
     # Create the constructor.
-    def __init__(self, inputSize, hiddenSize, Lee, chaotic = True):
+    def __init__(self, inputSize, hiddenSize, chaotic = True):
         # Create the super constructor.
         super(ChaoticAttention, self).__init__()
         # Get the chaotic controller.
         self.chaotic = chaotic
         # Create the linear layer.
         self.fc = nn.Linear(inputSize, hiddenSize)
-        # Create the Lee-Oscillator.
-        self.Lee = Lee
+        # Get the softmax.
+        self.softmax = nn.Softmax(dim = 1)
     
     # Create the forward propagation.
     def forward(self, x, h, c):
@@ -43,13 +40,8 @@ class ChaoticAttention(nn.Module):
             alpha[:, :, :x.shape[2]] = x
             alpha[:, :, x.shape[2]:] = torch.cat([h.unsqueeze(1), c.unsqueeze(1)], dim = 2)
         alpha = self.fc(alpha.reshape(-1, alpha.shape[2]))
-        # Check whether use the lee oscillator.
-        if self.chaotic == True:
-            #print("Chaotic: " + str(self.chaotic))
-            alpha = self.Lee.Sigmoid(alpha).reshape(x.shape[0], x.shape[1], -1).to(x.device)
-        else:
-            #print("Chaotic: " + str(self.chaotic))
-            alpha = torch.sigmoid(alpha).reshape(x.shape[0], x.shape[1], -1).to(x.device)
+        # Get the punishment.
+        alpha = self.softmax(alpha).reshape(x.shape[0], x.shape[1], -1).to(x.device)
         # Compute the context.
         context = torch.sum(alpha * x, dim = 1)
         # Return the context.
@@ -57,28 +49,18 @@ class ChaoticAttention(nn.Module):
 
 # Create the main function to test the Chaotic Attention Mechanism.
 if __name__ == "__main__":
-    # Get the Lee-Oscillator.
-    Lee = LeeOscillator()
-    # Create the Chaotic Attention Mechanism.
-    CAttention = ChaoticAttention(inputSize = 28, hiddenSize = 20, Lee = Lee)
-    # Test the Chaotic Attention Mechanism.
-    x = torch.randn((32, 9, 20))
-    hidden = (torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)))
+    # Create the Attention Mechanism.
+    CAttention = ChaoticAttention(inputSize = 30, hiddenSize = 10)
+    # Test the normal Attention Mechanism.
+    x = torch.randn((32, 9, 10))
+    hidden = (torch.zeros((32, 10)), torch.zeros((32, 10)))
     context = CAttention(x, hidden[0], hidden[1])
     print(context.shape)
 
-    # Create the normal Attention Mechanism.
-    CAttention = ChaoticAttention(inputSize = 28, hiddenSize = 20, Lee = Lee, chaotic = False)
+    # Create the Attention Mechanism.
+    CAttention = ChaoticAttention(inputSize = 20, hiddenSize = 10)
     # Test the normal Attention Mechanism.
-    x = torch.randn((32, 9, 20))
-    hidden = (torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)))
-    context = CAttention(x, hidden[0], hidden[1])
-    print(context.shape)
-
-    # Create the normal Attention Mechanism.
-    CAttention = ChaoticAttention(inputSize = 24, hiddenSize = 20, Lee = Lee, chaotic = False)
-    # Test the normal Attention Mechanism.
-    x = torch.randn((32, 9, 20))
-    hidden = (torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)), torch.zeros((32, 4)))
+    x = torch.randn((32, 9, 10))
+    hidden = (torch.zeros((32, 10)), torch.zeros((32, 10)))
     context = CAttention(x, hidden[0], None)
     print(context.shape)
