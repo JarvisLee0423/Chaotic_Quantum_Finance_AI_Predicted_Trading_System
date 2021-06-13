49152179640c0dedb7b22634ec1d7ad3804a2723
JarvisLee <609089938@qq.com>
2021-06-13 19:36:03 +0800

    Chaotic Encoder

--- Model/ChaoticEncoder.py
+++ Model/ChaoticEncoder.py
@@ -22,24 +22,34 @@ class ChaoticEncoder(nn.Module):
             - hiddenSize (integer), The output size of the Chaotic Encoder.\n
             - Lee (LeeOscillator), The Lee-Oscillator.\n
             - chaotic (bool), The boolean to check whether use the Chaotic Mode.\n
+            - bidirection (bool), The boolean to check whether apply the Bi-Model.\n
             - LSTM (bool), The boolean to check whether use the LSTM unit.\n
             - GRU (bool), The boolean to check whether use the GRU unit.\n
             - RNN (bool), The boolean to check whether use the RNN unit.\n
     '''
     # Create the constructor.
-    def __init__(self, inputSize, hiddenSize, Lee, chaotic = True, LSTM = False, GRU = False, RNN = False):
+    def __init__(self, inputSize, hiddenSize, Lee, chaotic = False, bidirection = False, LSTM = False, GRU = False, RNN = False):
         # Create the super constructor.
         super(ChaoticEncoder, self).__init__()
         # Create the Chaotic Encoder.
         if LSTM == True:
-            print("The Encoder applied LSTM unit.")
-            self.unit = ChaoticLSTM(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic)
+            if bidirection == True:
+                print("The Encoder applied Bi-LSTM unit.")
+            else:
+                print("The Encoder applied LSTM unit.")
+            self.unit = ChaoticLSTM(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic, bidirection = bidirection)
         elif GRU == True:
-            print("The Encoder applied GRU unit.")
-            self.unit = ChaoticGRU(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic)
+            if bidirection == True:
+                print("The Encoder applied Bi-GRU unit.")
+            else:
+                print("The Encoder applied GRU unit.")
+            self.unit = ChaoticGRU(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic, bidirection = bidirection)
         else:
-            print("The Encoder applied RNN unit.")
-            self.unit = ChaoticRNN(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic)
+            if bidirection == True:
+                print("The Encoder applied Bi-RNN unit.")
+            else:
+                print("The Encoder applied RNN unit.")
+            self.unit = ChaoticRNN(inputSize = inputSize, hiddenSize = hiddenSize, Lee = Lee, chaotic = chaotic, bidirection = bidirection)
     
     # Create the forward propagation.
     def forward(self, x):
@@ -52,8 +62,64 @@ class ChaoticEncoder(nn.Module):
 if __name__ == "__main__":
     # Get the Lee-Oscillator.
     Lee = LeeOscillator()
+    # Create the Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, bidirection = False, LSTM = True)
+    # Test the Encoder.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden[0].shape)
+    print(hidden[1].shape)
+
+    # Create the Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, bidirection = False, GRU = True)
+    # Test the Encoder.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden.shape)
+
+    # Create the Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, bidirection = False, RNN = True)
+    # Test the Encoder.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden.shape)
+
+    # Create the Bi-Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, bidirection = True, LSTM = True)
+    # Test the Bi-Encoder.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden[0].shape)
+    print(hidden[1].shape)
+
+    # Create the Bi-Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, bidirection = True, GRU = True)
+    # Test the Bi-Encoder.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden.shape)
+
+    # Create the Bi-Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, bidirection = True, RNN = True)
+    # Test the Bi-Encoder.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, hidden = CEncoder(x)
+    print(output.shape)
+    print(hidden.shape)
+
     # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, LSTM = True)
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = True, bidirection = False, LSTM = True)
     # Test the Chaotic Encoder.
     x = torch.randn((32, 10, 46))
     print(x.shape)
@@ -63,7 +129,7 @@ if __name__ == "__main__":
     print(hidden[1].shape)
 
     # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, GRU = True)
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = True, bidirection = False, GRU = True)
     # Test the Chaotic Encoder.
     x = torch.randn((32, 10, 46))
     print(x.shape)
@@ -72,7 +138,7 @@ if __name__ == "__main__":
     print(hidden.shape)
 
     # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, RNN = True)
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = True, bidirection = False, RNN = True)
     # Test the Chaotic Encoder.
     x = torch.randn((32, 10, 46))
     print(x.shape)
@@ -80,9 +146,9 @@ if __name__ == "__main__":
     print(output.shape)
     print(hidden.shape)
 
-    # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, LSTM = True)
-    # Test the Chaotic Encoder.
+    # Create the Chaotic Bi-Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = True, bidirection = True, LSTM = True)
+    # Test the Chaotic Bi-Encoder.
     x = torch.randn((32, 10, 46))
     print(x.shape)
     output, hidden = CEncoder(x)
@@ -90,18 +156,18 @@ if __name__ == "__main__":
     print(hidden[0].shape)
     print(hidden[1].shape)
 
-    # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, GRU = True)
-    # Test the Chaotic Encoder.
+    # Create the Chaotic Bi-Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = True, bidirection = True, GRU = True)
+    # Test the Chaotic Bi-Encoder.
     x = torch.randn((32, 10, 46))
     print(x.shape)
     output, hidden = CEncoder(x)
     print(output.shape)
     print(hidden.shape)
 
-    # Create the Chaotic Encoder.
-    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = False, RNN = True)
-    # Test the Chaotic Encoder.
+    # Create the Chaotic Bi-Encoder.
+    CEncoder = ChaoticEncoder(46, 10, Lee = Lee, chaotic = True, bidirection = True, RNN = True)
+    # Test the Chaotic Bi-Encoder.
     x = torch.randn((32, 10, 46))
     print(x.shape)
     output, hidden = CEncoder(x)
