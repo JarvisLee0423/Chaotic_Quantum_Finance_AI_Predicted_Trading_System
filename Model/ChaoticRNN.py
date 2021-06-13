3afa0313e1cabde7bcafbab980ead5d650b64024
JarvisLee <609089938@qq.com>
2021-06-13 19:35:07 +0800

    Chaotic Recurrent Neural Network Unit

--- Model/ChaoticRNN.py
+++ Model/ChaoticRNN.py
@@ -20,21 +20,31 @@ class ChaoticRNN(nn.Module):
             - hiddenSize (integer), The output size of the Chaotic RNN.\n
             - Lee (LeeOscillator), The Lee-Oscillator.\n
             - chaotic (bool), The boolean to check whether use the Chaotic Mode.\n
+            - bidirection (bool),  The boolean to check whether apply the Bi-RNN.\n
     '''
     # Create the constructor.
-    def __init__(self, inputSize, hiddenSize, Lee, chaotic = True):
+    def __init__(self, inputSize, hiddenSize, Lee, chaotic = False, bidirection = False):
         # Create the super constructor.
         super(ChaoticRNN, self).__init__()
         # Get the member variables.
         self.inputSize = inputSize
         self.hiddenSize = hiddenSize
         self.chaotic = chaotic
+        self.bidirection = bidirection
         # Create the parameter of the input.
         self.Wi = nn.Parameter(torch.Tensor(inputSize, hiddenSize))
         # Create the parameter of the hidden.
         self.Wh = nn.Parameter(torch.Tensor(hiddenSize, hiddenSize))
         # Create the parameter of the bias.
         self.B = nn.Parameter(torch.Tensor(hiddenSize))
+        # Check the number of directions.
+        if bidirection == True:
+            # Create the parameter of the input.
+            self.Winvi = nn.Parameter(torch.Tensor(inputSize, hiddenSize))
+            # Create the parameter of the hidden.
+            self.Winvh = nn.Parameter(torch.Tensor(hiddenSize, hiddenSize))
+            # Create the parameter of the bias.
+            self.Binv = nn.Parameter(torch.Tensor(hiddenSize))
         # Initialize the parameters.
         self.initParams()
         # Create the chaotic activation function.
@@ -56,9 +66,15 @@ class ChaoticRNN(nn.Module):
         output = []
         # Initialize the hidden.
         if initStates is None:
-            ht = torch.zeros(bs, self.hiddenSize).to(x.device)
+            if self.bidirection == True:
+                ht, hinvt = (torch.zeros(bs, self.hiddenSize).to(x.device), torch.zeros(bs, self.hiddenSize).to(x.device))
+            else:
+                ht = torch.zeros(bs, self.hiddenSize).to(x.device)
         else:
-            ht = initStates
+            if self.bidirection == True:
+                ht, hinvt = (initStates[:, :(self.hiddenSize // 2)], initStates[:, (self.hiddenSize // 2):])
+            else:
+                ht = initStates
         # Compute the RNN.
         for t in range(seqs):
             # Get the xt.
@@ -72,6 +88,22 @@ class ChaoticRNN(nn.Module):
                 ht = torch.tanh(gates).to(x.device)
             # Store the output value.
             output.append(ht.unsqueeze(1))
+        # Check the direction.
+        if self.bidirection == True:
+            for t in range(seqs - 1, -1, -1):
+                # Get the xinvt.
+                xinvt = x[:, t, :]
+                # Compute the gates.
+                gatesInv = xinvt @ self.Winvi + hinvt @ self.Winvh + self.Binv
+                # Get the value of the output.
+                if self.chaotic == True:
+                    hinvt = self.Lee.Tanh(gatesInv).to(x.device)
+                else:
+                    hinvt = torch.tanh(gatesInv).to(x.device)
+                # Store the output value.
+                output[t] = torch.cat([output[t], hinvt.unsqueeze(1)], dim = 2)
+            # Concatenate the hidden.
+            ht = torch.cat([ht, hinvt], dim = 1)
         # Concatenate the output.
         output = torch.cat(output, dim = 1)
         # Return the output and hidden.
@@ -81,19 +113,37 @@ class ChaoticRNN(nn.Module):
 if __name__ == "__main__":
     # Get the Lee-Oscillator.
     Lee = LeeOscillator()
-    # Create the Chaotic RNN unit.
-    CRNN = ChaoticRNN(inputSize = 4, hiddenSize = 10, Lee = Lee)
-    # Test the Chaotic RNN.
-    x = torch.randn((32, 9, 4))
+    # Create the Bi-RNN unit.
+    CRNN = ChaoticRNN(inputSize = 46, hiddenSize = 10, Lee = Lee, chaotic = False, bidirection = True)
+    # Test the Bi-RNN.
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, h = CRNN(x)
+    print(output.shape)
+    print(h.shape)
+
+    # Create the Chaotic Bi-RNN unit.
+    CRNN = ChaoticRNN(inputSize = 46, hiddenSize = 10, Lee = Lee, chaotic = True, bidirection = True)
+    # Test the Chaotic Bi-RNN.
+    x = torch.randn((32, 10, 46))
     print(x.shape)
     output, h = CRNN(x)
     print(output.shape)
     print(h.shape)
 
     # Create the Chaotic RNN unit.
-    CRNN = ChaoticRNN(inputSize = 4, hiddenSize = 10, Lee = Lee, chaotic = False)
+    CRNN = ChaoticRNN(inputSize = 46, hiddenSize = 10, Lee = Lee, chaotic = True, bidirection = False)
     # Test the Chaotic RNN.
-    x = torch.randn((32, 9, 4))
+    x = torch.randn((32, 10, 46))
+    print(x.shape)
+    output, h = CRNN(x)
+    print(output.shape)
+    print(h.shape)
+
+    # Create the RNN unit.
+    CRNN = ChaoticRNN(inputSize = 46, hiddenSize = 10, Lee = Lee, chaotic = False, bidirection = False)
+    # Test the RNN.
+    x = torch.randn((32, 10, 46))
     print(x.shape)
     output, h = CRNN(x)
     print(output.shape)
